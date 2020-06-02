00c1d1fb53ea6e68a0dc37e6eacfea5837ac180a
manzars <manzarshaikh69@gmail.com>
2020-06-03 03:08:06 +0530

    First Section Complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,109 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
+//Functional based component with state using hooks.
+// import React, { useState } from 'react';
+// const app = () => {
+
+//   const [personState, personStateChange] = useState({
+//     Person: [
+//       {
+//         name: "manzar",
+//         age: 21
+//       },
+//       {
+//         name: "samar",
+//         age: 22
+//       },
+//       {
+//         name: "xyz",
+//         age: 14
+//       }
+//     ]
+//   })
+
+//   const onClick = () =>{
+//     personStateChange({Person: personState.Person.filter(person => {
+
+//       if(person.name === "manzar"){
+//         person.age = 55;
+//       }
+//       return person
+
+//     })})
+//   }
+
+//   return (
+//     <div className="App">
+//       <button onClick = {onClick}>Hello</button>
+//       <Person name = {personState.Person[0].name} age = {personState.Person[0].age}/>
+//       <Person name = {personState.Person[1].name} age = {personState.Person[1].age}/>
+//       <Person name = {personState.Person[2].name} age = {personState.Person[2].age}> Hello world</ Person>
+//     </div>
+//   );
+// }
+// export default app;
+
+
+
+// class based component with state .
 class App extends Component {
+
+  state = {
+    Person: [
+      {
+        name: "manzar",
+        age: 21
+      },
+      {
+        name: "samar",
+        age: 22
+      },
+      {
+        name: "xyz",
+        age: 14
+      }
+    ]
+  }
+
+  onClick = (name) =>{
+    console.log("Was click")
+    this.setState({Person: this.state.Person.filter((person) =>{
+      if(person.name === "manzar"){
+        person.name = name;
+      }
+      return person
+    }) })
+  }
+
+  nameChange = (event) => {
+    this.setState({Person: this.state.Person.filter((person) =>{
+      if(person.age === 21){
+        person.name = event.target.value
+      }
+      return person
+    }) })
+  }
+
+  btnStyle = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '3px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  }
+
   render() {
     return (
-      <div className="App">
-        <Person />
-      </div>
-    );
+          <div className="App">
+            <button style={this.btnStyle} onClick = {() => this.onClick("abc")}>Hello</button>
+            <Person name = {this.state.Person[0].name} age = {this.state.Person[0].age}/>
+            <Person nameChanged = {this.nameChange} name = {this.state.Person[1].name} age = {this.state.Person[1].age}/>
+            <Person onHello = {this.onClick} name = {this.state.Person[2].name} age = {this.state.Person[2].age}> Hello world</ Person>
+          </div>
+        );
   }
 }
 
-export default App;
+export default App
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import "./Person.css"
 
-const Person = () => {
-    return <p> I'm a person</p>
+const Person = (props) => {
+    return (
+        <div className = "Person">
+            <p onClick = {() => props.onHello("holla")}> I'm a {props.name} and my age is {props.age}</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.nameChanged}/>
+        </div>
+    )
 }
 
 export default Person
+ 
